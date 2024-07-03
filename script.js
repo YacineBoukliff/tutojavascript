c19b6458fab398ea0ef0c62547f9f38029050d9c
yacine bouklif <yacinebouklif@hotmail.com>
2024-07-03 20:31:52 +0200

    exercice sur javascript

---
+++ script.js
@@ -0,0 +1,20 @@
+const ticket = {
+  nomFilm: "Batman",
+
+  prix: 15,
+
+  numeroSalle: 5,
+};
+console.log(ticket);
+
+let nom = "YacineBouklif";
+
+let affichage =
+  " Bonjour " +
+  nom +
+  " votre film " +
+  ticket.nomFilm +
+  " Est en salle " +
+  ticket.numeroSalle;
+
+console.log(affichage);
